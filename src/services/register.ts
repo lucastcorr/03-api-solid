067ffe4f7441c0564b449ae73bfc3d87ab2f35c2
lucastcorr <lucastcs95@gmail.com>
2023-04-04 19:27:28 -0300

    Create: first error class

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,5 +1,6 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 interface RegisterServiceRequest {
   name: string
@@ -25,7 +26,7 @@ export class RegisterService {
     const emailAlreadyExists = await this.usersRepository.findByEmail(email)
 
     if (emailAlreadyExists) {
-      throw new Error('Email already exists.')
+      throw new UserAlreadyExistsError()
     }
 
     await this.usersRepository.create({
