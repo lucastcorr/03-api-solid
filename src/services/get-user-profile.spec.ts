cdae08efe03276a622ff85435972b74edb3b8f90
lucastcorr <lucastcs95@gmail.com>
2023-04-09 14:46:38 -0300

    Created get-user-profile service and test

---
+++ src/services/get-user-profile.spec.ts
@@ -0,0 +1,43 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { hash } from 'bcryptjs'
+import { GetUserProfileService } from './get-user-profile'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileService
+
+describe('Get user profile Service', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileService(usersRepository)
+  })
+
+  it('Should be able to get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'Fulaninho',
+      email: 'fulaninho@gmail.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.executeGetUserProfileService({
+      userId: createdUser.id,
+    })
+
+    expect(user.name).toEqual('Fulaninho')
+  })
+
+  it('should not be able to get user profile with wrong id', async () => {
+    await usersRepository.create({
+      name: 'Fulaninho',
+      email: 'fulaninho123@gmail.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    expect(() =>
+      sut.executeGetUserProfileService({
+        userId: 'non-existing-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
---
+++ src/services/get-user-profile.ts
@@ -0,0 +1,29 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface GetUserProfileServiceRequest {
+  userId: string
+}
+
+interface GetUserProfileServiceResponse {
+  user: User
+}
+
+export class GetUserProfileService {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async executeGetUserProfileService({
+    userId,
+  }: GetUserProfileServiceRequest): Promise<GetUserProfileServiceResponse> {
+    const user = await this.usersRepository.findById(userId)
+
+    if (!user) {
+      throw new ResourceNotFoundError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
