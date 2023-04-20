6812667f52e1bd6fc824c1e599d25f65805f7cae
lucastcorr <lucastcs95@gmail.com>
2023-04-20 09:54:38 -0300

    Created Search Gyms service, tests and methods in gyms-repository

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -2,5 +2,6 @@ import { Gym, Prisma } from '@prisma/client'
 
 export interface GymsRepository {
   create(data: Prisma.GymCreateInput): Promise<Gym>
+  searchMany(query: string, page: number): Promise<Gym[]>
   findById(id: string): Promise<Gym | null>
 }
