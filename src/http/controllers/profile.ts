ccefa2212d9177ff7cf588a16d67ba5f7ffbe463
lucastcorr <lucastcs95@gmail.com>
2023-05-26 20:45:47 -0300

    Create profile controller

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,19 @@
+import { makeGetUserProfileService } from '@/services/factories/make-get-user-profile-service'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify()
 
-  console.log(request.user.sub)
+  const getUserProfile = makeGetUserProfileService()
 
-  return reply.status(200).send()
+  const { user } = await getUserProfile.executeGetUserProfileService({
+    userId: request.user.sub,
+  })
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
